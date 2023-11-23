90f08da1bce0bd16d44261830f14a46396601dec
Eduardo Flores <eduardoflorivas@gmail.com>
2023-11-23 12:28:22 -0600

    X3plus. Visualización del X3plus en RViz usando ROS2

---
+++ build/robot_pose_publisher/prefix_override/sitecustomize.py
@@ -0,0 +1,3 @@
+import sys
+sys.real_prefix = sys.prefix
+sys.prefix = sys.exec_prefix = '/home/rosuser/yahboom_ws/install/robot_pose_publisher'
---
+++ build/yahboom_app_save_map/prefix_override/sitecustomize.py
@@ -0,0 +1,3 @@
+import sys
+sys.real_prefix = sys.prefix
+sys.prefix = sys.exec_prefix = '/home/rosuser/yahboom_ws/install/yahboom_app_save_map'
